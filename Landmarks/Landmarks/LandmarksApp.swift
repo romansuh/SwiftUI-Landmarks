9a3639186e18ee19614c2fa75aeb82a0e9f89325
romansuh <roman@Romans-MacBook-Air.local>
2025-02-16 11:41:54 +0200

    initial commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Roman Sukhodoslkiy on 16.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmarks/LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,16 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by Roman Sukhodoslkiy on 16.02.2025.
+//
+
+import Testing
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
